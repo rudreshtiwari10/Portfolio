eb932f2e1f322885419b6db1bc319a48d86d8671
Rudresh tiwari <rudraprataptiwari786@gmail.com>
2026-02-12 18:14:19 +0530

    new project

--- test-build.js
+++
@@ -1,8 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-console.log('Testing imports...');
-try {
-  console.log('✓ React can be loaded');
-} catch(e) {
-  console.error('✗ React error:', e.message);
-}
